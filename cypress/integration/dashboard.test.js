8725806384cade29ba5cb24f3d6146411004a2e7
Japheth Nyandoro <japheth@limatech.co.ke>
2021-11-16 08:41:27 +0300

    chore: add comments to functions and classes

--- cypress/integration/dashboard.test.js
+++ cypress/integration/dashboard.test.js
@@ -1,30 +1,24 @@
-describe("Dashboard page", () => {
-  it("should display a list of tickers", () => {
-    cy.fixture("tickers.json").as("tickersJSON");
+describe('Dashboard page', () => {
+  it('should display a list of tickers', () => {
+    cy.fixture('tickers.json').as('tickersJSON');
     cy.server();
-    cy.route("assets/data/tickers.json", "@tickersJSON").as("tickers");
-    cy.visit("/");
-    cy.contains("Stock Manta");
-    cy.wait("@tickers");
-    cy.get(".ticker-item").should("have.length", 14);
+    cy.route('assets/data/tickers.json', '@tickersJSON').as('tickers');
+    cy.visit('/');
+    cy.contains('Stock Manta');
+    cy.wait('@tickers');
+    cy.get('.ticker-item').should('have.length', 14);
   });
 
-  it("should display a chart of time against end of day stock price", () => {
-    cy.fixture("tickercloseprice.json").as("tickerPriceJSON");
+  it('should display a chart of time against end of day stock price', () => {
+    cy.fixture('tickercloseprice.json').as('tickerPriceJSON');
     cy.request({
-      method: "GET",
-      url: "https://data.nasdaq.com/api/v3/datasets/WIKI/MSFT.json?collapse=annual&column_index=4&api_key=2sg42HWd2egYKWVwAqbb&order=asc",
+      method: 'GET',
+      url: 'https://data.nasdaq.com/api/v3/datasets/WIKI/MSFT.json?collapse=annual&column_index=4&api_key=2sg42HWd2egYKWVwAqbb&order=asc',
       failOnStatus: false,
     }).then((resp) => {
       expect(resp.status).to.eq(200);
     });
-    // cy.intercept(
-    //   "GET",
-    //   "https://data.nasdaq.com/api/v3/datasets/WIKI/MSFT.json?collapse=annual&column_index=4&api_key=2sg42HWd2egYKWVwAqbb&order=asc",
-    //   "@tickerPriceJSON"
-    // ).as("tickerprices");
-    cy.visit("/");
-    // cy.wait("@tickerprices");
-    cy.get("#tickerChart").should("have.length", 1);
+    cy.visit('/');
+    cy.get('#tickerChart').should('have.length', 1);
   });
 });
